e0ed1fe99a31d277c45a22ed49402cf5d81f2d9f
Fitzhugh <j.rfitzhugh@gmail.com>
2019-10-15 14:25:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -10,17 +10,20 @@ end
 
 def division(num1, num2)
   quotient = num1 / num2
-  qoutient
+  p quotient 
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  p product
 end
 
 def modulo(num1, num2)
-
+  remainder = num2 / num1
+  p remainder
 end
 
 def square_root(num)
-
+  root = num.sqrt
+  p root
 end
